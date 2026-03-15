b7e2474fa73a45484bb700f55b05d9ebcc0ea1b3
ChloeAmelinda <trangthichngunuong@gmail.com>
2026-03-15 22:16:09 +0700

    Add files via upload

---
+++ Session6/HW5/Ticket.java
@@ -0,0 +1,59 @@
+package Session6.HW5;
+
+public class Ticket {
+
+    private String ticketId;
+    private String roomName;
+
+    private boolean isSold = false;
+    private boolean isHeld = false;
+
+    private boolean isVIP;
+
+    private long holdExpiryTime;
+
+    public Ticket(String ticketId, String roomName) {
+        this.ticketId = ticketId;
+        this.roomName = roomName;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public boolean isSold() {
+        return isSold;
+    }
+
+    public void setSold(boolean sold) {
+        isSold = sold;
+    }
+
+    public boolean isHeld() {
+        return isHeld;
+    }
+
+    public void setHeld(boolean held) {
+        isHeld = held;
+    }
+
+    public long getHoldExpiryTime() {
+        return holdExpiryTime;
+    }
+
+    public void setHoldExpiryTime(long holdExpiryTime) {
+        this.holdExpiryTime = holdExpiryTime;
+    }
+
+    public boolean isVIP() {
+        return isVIP;
+    }
+
+    public void setVIP(boolean VIP) {
+        isVIP = VIP;
+    }
+}
