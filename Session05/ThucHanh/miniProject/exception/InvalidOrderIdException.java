eb8f02556d41eab36da09bbdca7a2e026894806a
ChloeAmelinda <trangthichngunuong@gmail.com>
2026-03-16 08:14:02 +0700

    Delete Session05/ThucHanh directory

--- Session05/ThucHanh/miniProject/exception/InvalidOrderIdException.java
+++
@@ -1,7 +0,0 @@
-package session05.miniProject.exception;
-
-public class InvalidOrderIdException extends Exception {
-    public InvalidOrderIdException(String message) {
-        super(message);
-    }
-}
--- Session05/ThucHanh/miniProject/modal/OrderItem.java
+++
@@ -1,8 +0,0 @@
-package session05.miniProject.modal;
-
-public record OrderItem(MenuItem item, int quantity) {
-
-    public double subtotal() {
-        return item.calculatePrice() * quantity;
-    }
-}
