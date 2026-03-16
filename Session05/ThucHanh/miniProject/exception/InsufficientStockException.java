895587e71971ae835687b34b610fd7a3f33cb629
@Changiuoi <trangthichngunuong@gmail.com>
2026-03-16 08:29:36 +0700

    tên thay đổi

--- Session05/ThucHanh/miniProject/exception/InsufficientStockException.java
+++
@@ -1,7 +0,0 @@
-package session05.miniProject.exception;
-
-public class InsufficientStockException extends Exception {
-    public InsufficientStockException(String message) {
-        super(message);
-    }
-}
--- Session1/HW5/InvalidAgeException.java
+++
@@ -1,7 +0,0 @@
-package Session1.HW5;
-
-public class InvalidAgeException extends Exception{
-    public InvalidAgeException(String mgs){
-        super(mgs);
-    }
-}
